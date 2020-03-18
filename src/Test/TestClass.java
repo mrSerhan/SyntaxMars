02af6a2842e14608b9b7254798c1da4a149a4ec8
mrSerhan <mrserhan@hotmail.com>
2020-03-17 19:31:28 -0500

    created project and class

--- src/Test/TestClass.java
+++ src/Test/TestClass.java
@@ -3,14 +3,7 @@ package Test;
 public class TestClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(" hello world");
-		
-		int [] a = {1,2,3};
-		for(int i=0;i<a.length;i++) {
-			System.out.println(a[i]);
-		}
-		System.out.println("second change");
+		System.out.println();
 	}
 
 }
