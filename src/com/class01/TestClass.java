1920372ce9049fdfe5a25cac5b818ea8caa3d8ce
mrSerhan <mrserhan@hotmail.com>
2020-03-17 20:08:19 -0500

    create new com.class2 and day2.java

--- src/com/class01/TestClass.java
+++ src/com/class01/TestClass.java
@@ -1,4 +1,4 @@
-package Test;
+package com.class01;
 
 public class TestClass {
 
--- src/com/class02/Day2.java
+++ src/com/class02/Day2.java
@@ -1,6 +1,6 @@
-package Test;
+package com.class02;
 
-public class Test02 {
+public class Day2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
