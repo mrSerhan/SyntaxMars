5ddbc038b413eec772ce58733cbbf5cc9747ce14
mrSerhan <mrserhan@hotmail.com>
2020-03-17 20:12:21 -0500

    if statement added to class day2

--- src/com/class02/Day2.java
+++ src/com/class02/Day2.java
@@ -5,6 +5,11 @@ public class Day2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		boolean isRaining = true;
+		
+		if (isRaining) {
+			System.out.println("it is raining");
+		}
 	}
 
 }
